ba415689c0634d2f5c38f44be3fbdc69a466c181
nskun <8086.nk@gmail.com>
2015-01-09 22:36:21 +0900

    色々修正

--- brainfuck.js
+++ brainfuck.js
@@ -1,67 +1,82 @@
-
 // 現在のIPの位置
 var ip = 0;
 // 現在の値
-var reg = new Array(100);
+var reg = new Array();
+// getcharの位置
+var getcharPointer = 0;
+// input-textの参照を定義
+var inputText = document.getElementById("input-text");
 
-// 初期化
-for (var i = 0; i < 100; i++) {
-    reg[i]=0;
+init(100);
 
+function chenge() {
+
+}
+
+function init() {
+	// 初期化
+	for (var i = 0; i < index; i++) {
+		reg[i]=0;
+	}
+    display();
 }
 
 function pointer() {
-    // 入力された数
-    var index = document.getElementById("index").value;
-
-    var pointer = "";
-    var i = 0;
-    for (var val in reg) {
-        pointer+='<span id="pointer' + i++ + '">' + reg[val] + '</span>\n';
-    }
-    alert(pointer);
-    // 表示する
-    document.getElementById("pointer").innerHTML= pointer;
+	// 入力された数
+	var index = document.getElementById("index").value;
+	display();
 }
 
-/*
-* インクリメント
-*/
-function inc() {
-
+function getchar() {
+	return inputText.value.substr(getcharPointer++,1);
 }
+
+function display() {
+	var pointer = "";
+	var i = 0;
+	for (var val in reg) {
+		$hex = (reg[val] < 16) ? "0" + reg[val].toString(16) : reg[val].toString(16);
+		pointer+='<span id="' + i++ + '">' + $hex + '</span>\n';
+	}
+	// 表示する
+	document.getElementById("pointer").innerHTML = pointer;
+}
+
 /*
  実行
  */
 function execute() {
-    var PG = document.getElementById("PG").value;
-    // バリデーションチェック
-
-    // 実行
-    switch (PG.charAt(0)){
-        case '<':
-            inc();
-            break;
-        case '>':
-            dec();
-            break;
-        case '+':
-            pinc();
-            break;
-        case '-':
-            pdec();
-            break;
-        case '.':
-            output();
-            break;
-        case ',':
-            input();
-            break;
-        case '[':
-            start-jmp();
-            break;
-        case ']':
-            end-jmp();
-            break;
-    }
+	var PG = document.getElementById("PG").value;
+	// バリデーションチェック
+	// 実行
+	for (var i = 0; i < PG.length; i++) {
+		switch (PG[i].charAt(0)) {
+			case '<':
+				ip++;
+				break;
+			case '>':
+				ip--;
+				break;
+			case '+':
+				(reg[ip] == 255) ? reg[ip]=0 : reg[ip]++;
+				break;
+			case '-':
+				(reg[ip] == 0) ? reg[ip] = 255 : reg[ip]--;
+				break;
+			case '.': // output
+				var id = "pointer" + ip;
+				document.getElementById(id).innerHTML = reg[ip];
+				break;
+			case ',': // input
+				reg[ip] = getchar();
+				break;
+			//case '[':
+			//    start-jmp();
+			//    break;
+			//case ']':
+			//    end-jmp();
+			//    break;
+		}
+	}
+	display();
 }
